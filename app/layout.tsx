3570efd128f6fd2d7d0b245a0e040e1cf4c5b2c3
Deima <deimas.0d@gmai.com>
2025-12-05 19:59:57 -0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 export const metadata: Metadata = {
   metadataBase: new URL('https://homeroofprogram.com'),
   title: {
-    default: 'Home Roof Program | Official Eligibility Portal',
+    default: 'Home Roof Program | National Eligibility Finder',
     template: '%s | HRP'
   },
   description: 'Secure verification portal for homeowners insurance policy allowances. Check eligibility for roof restoration benefits in your region.',
@@ -73,13 +73,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2805659046165874');
+            fbq('init', '1174860684795377');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=2805659046165874&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1174860684795377&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
